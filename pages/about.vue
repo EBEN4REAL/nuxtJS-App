d4a4749ce704a8839d4e57155fb652671495937e
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-05-06 11:31:29 +0100

    ..

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,26 @@
 <template>
-    
+    <div>
+        <h1>About Dadjokes</h1>
+        <p>This is an app that deploys corny dad</p>
+    </div>
     
 </template>
 
 <script>
-
+    export default {
+        head() {
+            return {
+                title: 'About The App',
+                meta: [
+                    {
+                        hid: "description",
+                        name: "description",
+                        content: "Best place for corny dad Jokes"
+                    }
+                ]
+            }
+        }
+    }
 
 </script>
 
