d1a65030eff14977fdaed5c2a3f21152af9a84b9
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-05-06 13:50:49 +0100

    ..

---
+++ pages/jokes/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+       Jokes
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        head() {
+            return {
+                title: 'About The App',
+                meta: [
+                    {
+                        hid: "description",
+                        name: "description",
+                        content: "Best place for corny dad Jokes"
+                    }
+                ]
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
